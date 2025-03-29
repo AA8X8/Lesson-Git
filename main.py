81687199b116bc133b6ffbf4cd84e6c2edce3c49
Alina Abdullaeva <areon.aur@mail.ru>
2025-03-29 16:55:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 print("Hello world")
+
+def merge(a, b):
+    result = a1.update(b1)
+    return (result)
+
+a1 = {'Koshelev' : 'frontender'} 
+b1 = {'Alina' : 'frontender'} 
+merge(a1, b1)
+print("Merged Dictionaries is : ") 
+
+
+for i in range(8):
+    a = "Koshelev is not a frontender".split()
+    print(list(a1.items())[1][0], a[1], a[3], a[4], sep=" ")
